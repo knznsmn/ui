7cf0902fc3b2d737d7fb1e45b50d1c8c16751da6
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-05-06 17:49:39 +0400

    Add Challenge 004 data

--- assets/js/modules/challenges.js
+++ assets/js/modules/challenges.js
@@ -10,6 +10,10 @@ export const challenges = [
 	{
 		title: "Prompt: Landing Page",
 		description: "What's the main focus? Is it for a book, an album, a mobile app, a SaaS product? Consider important landing page elements (Headlines, call-to-action buttons, typography, clarity, etc.)"
+	},
+	{
+		title: "Prompt: Calculation",
+		description: "Design a calculation element or interface. Is it a standard calculator, a scientific one, or specialty calculator for something such as a home mortgage or auto loan? Is it to forecast a calculation such as for a credit score? Is it for a phone, a tablet, a web app?"
 	}
 
 ]
